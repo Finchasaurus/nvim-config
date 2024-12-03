f7f49f98501d9038b1e2372e151892b226922963
Theo <finchasaurus@gmail.com>
2024-12-03 16:51:17 -0700

    colors

--- lua/plugins/ui/catppuccin.lua
+++ lua/plugins/ui/catppuccin.lua
@@ -1,17 +1,5 @@
 -- catppuccin.lua
-local base = {
-    red = "#ff657a",
-    maroon = "#F29BA7",
-    peach = "#ff9b5e",
-    yellow = "#eccc81",
-    green = "#a8be81",
-    teal = "#9cd1bb",
-    sky = "#A6C9E5",
-    sapphire = "#86AACC",
-    blue = "#5d81ab",
-    lavender = "#66729C",
-    mauve = "#b18eab"
-}
+local base = require("tables.colors")
 
 local extend_base = function(value)
     return vim.tbl_extend("force", base, value)
---
+++ lua/tables/colors.lua
@@ -0,0 +1,15 @@
+local colors = {
+    red = "#ff657a",
+    maroon = "#F29BA7",
+    peach = "#ff9b5e",
+    yellow = "#eccc81",
+    green = "#a8be81",
+    teal = "#9cd1bb",
+    sky = "#A6C9E5",
+    sapphire = "#86AACC",
+    blue = "#5d81ab",
+    lavender = "#66729C",
+    mauve = "#b18eab"
+}
+
+return colors
