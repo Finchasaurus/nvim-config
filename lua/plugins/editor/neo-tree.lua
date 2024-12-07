93637de6b3539f332c8e2f8015b176188c7dc497
Theo <finchasaurus@gmail.com>
2024-12-06 21:24:57 -0700

    menits

--- lua/plugins/editor/neo-tree.lua
+++ lua/plugins/editor/neo-tree.lua
@@ -12,4 +12,44 @@ return {
 			position = "float",
 		},
 	},
+	keys = {
+		{
+			"<leader>fe",
+			function()
+				require("neo-tree.command").execute({ toggle = true, dir = vim.uv.cwd() })
+			end,
+			desc = "Explorer NeoTree",
+		},
+		{
+			"<leader>ge",
+			function()
+				require("neo-tree.command").execute({ source = "git_status", toggle = true })
+			end,
+			desc = "Git Explorer",
+		},
+		{
+			"<leader>be",
+			function()
+				require("neo-tree.command").execute({ source = "buffers", toggle = true })
+			end,
+			desc = "Buffer Explorer",
+		},
+	},
+	init = function()
+		vim.api.nvim_create_autocmd("BufEnter", {
+			group = vim.api.nvim_create_augroup("Neotree_start_directory", { clear = true }),
+			desc = "Start Neo-tree with directory",
+			once = true,
+			callback = function()
+				if package.loaded["neo-tree"] then
+					return
+				else
+					local stats = vim.uv.fs_stat(vim.fn.argv(0))
+					if stats and stats.type == "directory" then
+						require("neo-tree")
+					end
+				end
+			end,
+		})
+	end,
 }
