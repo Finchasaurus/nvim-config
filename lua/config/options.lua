a1a1f9fc70d5f4cf7511a7212bf3529420ab37ed
Theo <finchasaurus@gmail.com>
2025-01-25 21:20:41 -0700

    bufferline + opts

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,3 +4,4 @@ vim.opt.relativenumber = false
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
 vim.opt.number = true
+vim.wo.wrap = false
---
+++ lua/plugins/ui/bufferline.lua
@@ -0,0 +1,17 @@
+return {
+	"akinsho/bufferline.nvim",
+	version = "*",
+	dependencies = {"echansnovski/mini.icons", "tiagovla/scope.nvim"},
+	keys = {
+		{ "<leader>br", "<cmd>BufferLineCloseRight", desc = "Close buffers to the right" },
+		{ "<leader>bl", "<cmd>BufferLineCloseLeft", desc = "Close buffers to the left" },
+		{ "<S-h>", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev Buffer" },
+		{ "<S-l>", "<cmd>BufferLineCycleNext<cr>", desc = "Next Buffer" }
+	},
+	opts = {
+
+	},
+	config = function(_, opts)
+		require("bufferline").setup(opts)
+	end
+}
