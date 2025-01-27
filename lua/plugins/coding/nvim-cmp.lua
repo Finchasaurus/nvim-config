6452b048edaa0904af9aa3958f5fcf82b04e11d1
Theo <finchasaurus@gmail.com>
2025-01-26 18:00:00 -0700

    nvim cmp

--- lua/plugins/coding/nvim-cmp.lua
+++ lua/plugins/coding/nvim-cmp.lua
@@ -6,4 +6,50 @@ return {
 		"hrsh7th/cmp-buffer",
 		"hrsh7th/cmp-path",
 	},
+	opts = function()
+		--vim.api.nvim_set_h1(0, "CmpGhostText", { link = "Comment", default = true })
+		local cmp = require("cmp")
+		local defaults = require("cmp.config.default")
+		local autoselect = true
+
+		return {
+			auto_brackets = { "lua", "typescript" },
+			completion = {
+				completeopt = "menu,menuone,noinsert" .. (autoselect and "" or ",noselect"),
+			},
+			preselect = autoselect and cmp.PreselectMode.Item or cmp.PreselectMode.None,
+			mapping = cmp.mapping.preset.insert({
+				["<C-b>"] = cmp.mapping.scroll_docs(-4),
+				["<C-f>"] = cmp.mapping.scroll_docs(4),
+				["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+				["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+				["<C-r>"] = cmp.mapping.confirm({ select = true }),
+				["<tab>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_next_item()
+					elseif vim.snippet.active({ direction = 1 }) then
+						vim.snippet.jump(1)
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
+				["<S-tab>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_next_item()
+					elseif vim.snippet.active({ direction = -1 }) then
+						vim.snippet.jump(-1)
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
+			}),
+			sources = cmp.config.sources({
+				{ name = "lazydev" },
+				{ name = "nvim_lsp" },
+				{ name = "path" },
+			}, {
+				{ name = "buffer" },
+			}),
+		}
+	end,
 }
